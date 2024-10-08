b1f0dabd8411f7b7fde2a72209ff8107e56e264f
Marcin <marsobpro@gmail.com>
2024-10-08 16:21:07 +0200

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
