a292ddfcb5fd3f09ac8821c9bca688e42781b356
Marcin <marsobpro@gmail.com>
2024-10-13 12:57:26 +0200

    Added bg img and content for cards

--- src/components/Tagline.jsx
+++ src/components/Tagline.jsx
@@ -3,7 +3,7 @@ import brackets from "../assets/svg/Brackets";
 
 const Tagline = ({ className, children }) => {
   return (
-    <div className={`${className || ""}`}>
+    <div className={`flex items-center ${className || ""}`}>
       {brackets("left")}
       <div className="mx-3 text-n-3">{children}</div>
       {brackets("right")}
