0a01f3d387253bb801f420667fc383576881173c
Marcin <marsobpro@gmail.com>
2024-10-13 11:56:34 +0200

    Added the rest of the bento boxes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
-import { brainwaveServices } from "../constants";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
-import { PhotoChatMessage } from "./design/Services";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -52,7 +57,7 @@ const Services = () => {
           </div>
 
           {/* Second service */}
-          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2 mb-5">
             <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 <img
@@ -70,6 +75,54 @@ const Services = () => {
                   editing feature. Try it now!
                 </p>
               </div>
+              <PhotoChatMessage />
+            </div>
+          </div>
+
+          {/* Third service */}
+          <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem] ">
+            <div className="py-12 px-4 xl:px-8">
+              <h4 className="h4 mb-4">Video generation</h4>
+              <p className="body-2 mb-[2rem] text-n-3">
+                The world’s most powerful AI photo and video art generation
+                engine. What will you create?
+              </p>
+
+              <ul className="flex items-center justify-between">
+                {brainwaveServicesIcons.map((item, index) => (
+                  <li
+                    className={`flex items-center justify-center rounded-2xl ${
+                      index === 2
+                        ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                        : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                    }`}
+                    key={index}
+                  >
+                    <div
+                      className={`${
+                        index === 2
+                          ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                          : ""
+                      }`}
+                    >
+                      <img src={item} width={24} height={24} alt={item} />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+              <img
+                src={service3}
+                className="w-full h-full object-cover"
+                width={520}
+                height={400}
+                alt="Robor"
+              />
+
+              <VideoChatMessage />
+              <VideoBar />
             </div>
           </div>
         </div>
